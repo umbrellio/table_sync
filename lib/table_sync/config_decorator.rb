f41991dc25a3de326fd2581ba1e59613f2491a91
Rustam Ibragimov <iamdaiver@gmail.com>
2019-10-28 13:30:47 +0300

    [wrap-receiving] fixed typo in delegation

--- lib/table_sync/config_decorator.rb
+++ lib/table_sync/config_decorator.rb
@@ -6,7 +6,7 @@ module TableSync
     include ::TableSync::EventActions
     extend Forwardable
 
-    def_delegators :@config, :on_destroy, :wrap_receving
+    def_delegators :@config, :on_destroy, :wrap_receiving
 
     def initialize(config, handler)
       @config = config
