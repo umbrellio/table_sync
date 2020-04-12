0fe4183f7f0342a5d916c6ac956d97ab730a71bb
Rustam Ibragimov <iamdaiver@gmail.com>
2020-04-12 17:51:38 +0300

    [gem] bump to 2.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
-## [Unreleased]
+## [2.2.0] - 2020-04-12
 ### Added
 - Introduce Plugin ecosystem (**TableSync::Plugins**);
 
--- lib/table_sync/plugins/access_mixin.rb
+++ lib/table_sync/plugins/access_mixin.rb
@@ -26,7 +26,7 @@ module TableSync::Plugins::AccessMixin
     TableSync::Plugins.names
   end
 
-  # @return [Hash<String,Class<TableSync::Plugins::Abstract>]
+  # @return [Hash<String,Class<TableSync::Plugins::Abstract>>]
   #
   # @api private
   # @since 2.2.0
