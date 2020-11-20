9b18c80507f9e77098ebccd6dcfc24a9bfe1fbb6
tdrive <tdrive@xc0de.ru>
2020-11-20 16:06:10 +0300

    dev

--- lib/table_sync/receiving/handler.rb
+++ lib/table_sync/receiving/handler.rb
@@ -110,10 +110,10 @@ class TableSync::Receiving::Handler < Rabbit::EventHandler
 
     keys_sample = data[0].keys
     keys_diff = data.each_with_object(Set.new) do |row, set|
-      (row.keys - keys_sample | keys_sample - row.keys).each(&set.method(:add)) 
+      (row.keys - keys_sample | keys_sample - row.keys).each(&set.method(:add))
     end
 
-    if keys_diff.size > 0
+    unless keys_diff.empty?
       raise TableSync::DataError.new(data, target_keys, <<~MESSAGE)
         Bad batch structure, check keys: #{keys_diff.to_a}
       MESSAGE
